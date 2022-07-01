c4108b1ea05bb01092b148b564069313b449a9b6
steph bug <plexikongit@gmail.com>
2022-07-01 09:03:21 +0200

    tests

---
+++ tests/Unit/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chronhub\Messager\Tests\Unit;
+
+use Chronhub\Messager\Tests\TestCase;
+
+final class DummyTest extends TestCase
+{
+    public function it_assert_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
